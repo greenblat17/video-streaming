862dacc510011d1062560239f398d8f332d56991
Alex Zhuravlev <leftemail17@mail.ru>
2023-11-26 19:30:16 +0300

    ref: replaced uuid to unique filename

--- src/main/kotlin/com/rbi/service/impl/DefaultVideoService.kt
+++ src/main/kotlin/com/rbi/service/impl/DefaultVideoService.kt
@@ -4,6 +4,8 @@ import com.rbi.repository.FileMetadataRepository
 import com.rbi.exception.StorageException
 import com.rbi.model.FileMetadataEntity
 import com.rbi.service.VideoService
+import com.rbi.util.FileUtil.Companion.generateFileNameWithExtension
+import com.rbi.util.FileUtil.Companion.getExtension
 import com.rbi.util.Range
 import io.ktor.http.content.*
 import java.io.ByteArrayInputStream
@@ -16,36 +18,40 @@ class DefaultVideoService(
 
     override suspend fun save(video: PartData.FileItem): UUID {
         val fileUUID = UUID.randomUUID()
+
         val videoBytes = ByteArrayInputStream(video.streamProvider().readBytes())
+        val fileName = video.originalFileName.toString()
 
         val metadata = FileMetadataEntity(
             fileUUID.toString(),
             videoBytes.available().toLong(),
+            getExtension(fileName),
             video.contentType.toString()
         )
 
         fileMetadataRepository.save(metadata)
-        storageService.save(video, videoBytes, fileUUID)
+        storageService.save(fileName, videoBytes, fileUUID)
 
         return fileUUID
     }
 
     override suspend fun fetchChunk(uuid: UUID, range: Range): ChunkWithMetadata {
         val fileMetadata = fileMetadataRepository.findById(uuid)
-        return ChunkWithMetadata(fileMetadata!!, readChunk(uuid, range, fileMetadata.size))
+        val fileName = generateFileNameWithExtension(uuid, fileMetadata!!.extension)
+        return ChunkWithMetadata(fileMetadata, readChunk(fileName, range, fileMetadata.size))
     }
 
     override fun getFullVideo(uuid: UUID): ByteArray {
         return storageService.getObject(uuid.toString())
     }
 
-    private fun readChunk(uuid: UUID, range: Range, fileSize: Long): ByteArray {
+    private fun readChunk(fileName: String, range: Range, fileSize: Long): ByteArray {
         val startPosition = range.getRangeStart()
         val endPosition = range.getRangeEnd(fileSize)
         val chunkSize = (endPosition - startPosition + 1)
 
         try {
-            storageService.getInputStream(uuid, startPosition, chunkSize).use { inputStream ->
+            storageService.getInputStream(fileName, startPosition, chunkSize).use { inputStream ->
                 return inputStream.readAllBytes()
             }
         } catch (exception: Exception) {
